1b22a0351fe785fb4ed239b5d9a3184a5998f514
Lauren Gibbons <gibbons.l@outlook.com>
2023-03-26 17:54:19 -0400

    CSS changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router';
+import "./assets/home.scss";
 
 createApp(App).use(router).mount('#app')
 
